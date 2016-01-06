842c695a4859706853e2ea75b376959d69e72082
Scott <Scott@10.68.224.153>
2016-01-06 11:38:41 +0100

    blubjes

--- java12/src/java12/Opdracht6.java
+++ java12/src/java12/Opdracht6.java
@@ -5,20 +5,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.applet.*;
 
-public class Opdracht5 extends Applet {
+public class Opdracht6 extends Applet {
 	
     boolean gevonden;
-    double[] salaris = { 100.0, 100.0, 100.0, 200.0, 500.0, 400.0, 400.0,  300.0 }; 
+    double[] salaris = { 100.0, 200.0, 500.0, 400.0, 300.0 }; 
     double gezocht;
-    int zelfde;
     Button knop;
     TextField tekstvak;
     String s;
 	
     public void init() {
     	setSize(600, 600);
-    	
-    	zelfde = 0;
     	
     	knop = new Button("Ok");
     	knop.addActionListener(new KnopLuisteraar());
@@ -32,9 +29,8 @@ public class Opdracht5 extends Applet {
 
     public void paint(Graphics g) {
         if(gevonden == true) {
-            g.drawString("De waarde is " + zelfde + "x gevonden", 20, 50); 
+            g.drawString("De waarde is gevonden.", 20, 50);
             gevonden = false;
-            zelfde = 0;
         }
         else {
             g.drawString("De waarde is niet gevonden.", 20, 50);
@@ -48,7 +44,6 @@ public class Opdracht5 extends Applet {
             while(teller < salaris.length) {
                 if(salaris[teller] == gezocht) {
                     gevonden = true;
-                   zelfde++;
                 }
                 teller++;
             }
